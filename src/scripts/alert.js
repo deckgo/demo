898e025357a17424f8137d2067da514c4d743652
daviddalbusco <david.dalbusco@outlook.com>
2018-12-06 21:33:23 +0100

    fix: weird square

--- src/scripts/alert.js
+++ src/scripts/alert.js
@@ -10,7 +10,7 @@ presentAlert = async () => {
     const alert = await alertController.create({
         header: 'Hey!',
         subHeader: 'Nice to meet you',
-        message: 'To navigate, just swipe the slides, use the left and right arrows keys of your keyboard or use the actions available at the bottom right.️',
+        message: 'To navigate, just swipe the slides, use the left and right arrows keys of your keyboard or use the actions available at the bottom right',
         buttons: ['OK']
     });
 
