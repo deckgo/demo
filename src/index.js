aabf95fc8909e2c35926830e48584951680d23bd
peterpeterparker <david.dalbusco@outlook.com>
2020-01-11 09:58:20 +0100

    fix: youtube component not correctly loaded

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ import { defineCustomElements as deckDeckGoChartsElements } from '@deckdeckgo/ch
 import { defineCustomElements as deckDeckGoQRCodeElements } from '@deckdeckgo/qrcode/dist/loader';
 import { defineCustomElements as deckDeckGoHighlightCodeElements } from '@deckdeckgo/highlight-code/dist/loader';
 import { defineCustomElements as deckDeckGoSocialElements } from '@deckdeckgo/social/dist/loader';
-import { defineCustomElements as deckDeckGoYoutubeElements } from '@deckdeckgo/social/dist/loader';
+import { defineCustomElements as deckDeckGoYoutubeElements } from '@deckdeckgo/youtube/dist/loader';
 
 // Init web-social-share
 import { defineCustomElements as webSocialShareElements } from 'web-social-share/dist/loader';
